781abd4736b53233bf4f53c102a396e00a1adc13
skumardas267 <skumardas267@gmail.com>
2021-07-19 11:48:37 +0530

    "threading added"

--- Mainclass.java
+++ Mainclass.java
@@ -1,16 +1,15 @@
 package project;
 
-public class Mainclass {
+public class Mainclass extends Thread{
 
 	private static final String String = null;
 
-	public static void main( String args[] )
-    {
+	public static void main( String args[] ) throws InterruptedException {
       
 		
 	
 	bmi a= new bmi();
-	patient_details b= new patient_details();
+	project.patient_details b= new project.patient_details();
 	medical_parameteres c= new medical_parameteres();
 	
 	
@@ -26,6 +25,7 @@ public class Mainclass {
 
 	float ans=b.details_points(b.age,b.gender);
 	System.out.println(ans);
+	Thread.sleep(5000);
 	b.display();
 	c.Inflammatory_Details_input();
 	float ans2=c.inflammatory_points(c.Dyspnea,c.N_count,c.L_count,c.CRP);
